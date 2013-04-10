156eec8001cd06a6140089cb6298f744f824c14b
Jonathan Mellman <jonathan.mellman@furman.edu>
2013-04-10 15:24:48 +0200

    Initial commit after running 'rails new' command

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CourseShopper::Application.config.secret_token = 'aada91dc1ae2754760e24961d0426da2dc256c7bb5e831a06f899d932db9534960258d8642b2124c42fb142bdbe9a25976ef9a8cb9349db6228b5f964c661d29'
