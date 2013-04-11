c757a0416e4d66d03bf15cc7d86d19c0304c4b65
Jonathan Mellman <jonathan.mellman@furman.edu>
2013-04-11 12:52:33 +0200

    db/seeds.rb correctly seeds data from app/assets/courses.xml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+courses = Nokogiri::XML(open("app/assets/courses.xml"))
+
+@seperator = ", " #to seperate multiple instructors or programmes
+
+# loop through courses, grab attributes, and add the course to the database
+courses.xpath('//courses/course').each_with_index do |l, i|
+  
+  catalog_number = l.at_xpath('@sgid').text.strip
+  title = l.at_xpath('name').text.strip
+  credits = l.at_xpath('ects').text.strip
+  institute = l.at_xpath('institute').text.strip
+  
+  #grab instructors
+  staff = ""
+  l.xpath('staff_list/staff').each do |s|
+    if staff.empty?  
+      staff += s.xpath('name').text.strip
+    else
+      staff += @seperator + s.xpath('name').text.strip
+    end
+  end
+  
+  #grab programmes
+  programmes = ""
+  l.xpath('programmes/programme').each do |p|
+    if programmes.empty?  
+      programmes += p.xpath('name').text.strip
+    else
+      programmes += @seperator + p.xpath('name').text.strip
+    end
+  end
+    
+  description = l.at_xpath('description').text.strip
+  participant_count = l.at_xpath('participant_count').text.strip
+  
+  #create course
+  Course.create(:catalog_number    => catalog_number,
+                :title             => title, 
+                :institute         => institute, 
+                :credits           => credits,
+                :description       => description,
+                :staff             => staff,
+                :programmes        => programmes,                
+                :participant_count => participant_count
+                )  
+end
